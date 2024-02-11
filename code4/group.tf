843a389ee9ef9e8d82ed274f2facf54db6f6d713
Mariam <tosynecutie@gmail.com>
2024-02-11 07:48:13 -0500

    terraform code

---
+++ code4/group.tf
@@ -0,0 +1,5 @@
+
+resource "aws_iam_group" "cloudteam" {
+  name = "cloudteam"
+  
+}
---
+++ code4/output.tf
@@ -0,0 +1,9 @@
+
+output "instance_ip" {
+    value = aws_lightsail_instance.custom.public_ip_address
+  
+}
+output "private_ip" {
+    value = aws_lightsail_instance.custom.private_ip_address
+  
+}
